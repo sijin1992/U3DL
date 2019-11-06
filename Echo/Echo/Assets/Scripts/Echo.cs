8bb3aafc7456a5b13da67cddbe63c6a449958b38
JX-PC\JX <sijin1992@sina.com>
2019-11-06 17:12:42 +0800

    Echo客户端和服务器

---
+++ Echo/Echo/Assets/Scripts/Echo.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using UnityEngine.UI;
+
+public class Echo : MonoBehaviour {
+    //定义套接字
+    Socket socket;
+    //UGUI
+    [SerializeField] private Button Connbutton;
+    [SerializeField] private Button Sendbutton;
+    public InputField InputFeld;
+    public Text text;
+
+    private void Start()
+    {
+        Connbutton.onClick.AddListener(Connection);
+        Sendbutton.onClick.AddListener(Send);
+    }
+
+    //点击连接按钮
+    public void Connection()
+    {
+        //Socket
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        //Connect
+        socket.Connect("127.0.0.1", 8888);
+    }
+    //点击发送按钮
+    public void Send()
+    {
+        //Send
+        string sendStr = InputFeld.text;
+        byte[] sendBytes = System.Text.Encoding.Default.GetBytes(sendStr);
+        socket.Send(sendBytes);
+        //Recv
+        byte[] readBuff = new byte[1024];
+        int count = socket.Receive(readBuff);
+        string recvStr = System.Text.Encoding.Default.GetString(readBuff, 0, count);
+        text.text = recvStr;
+        //Close
+        socket.Close();
+    }
+}
---
+++ Echo/EchoServer/EchoServer/EchoServer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace EchoServer
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            //Socket
+            Socket Listenfd = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //Bind
+            IPAddress ipAdr = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipEp = new IPEndPoint(ipAdr, 8888);
+            Listenfd.Bind(ipEp);
+            //Listen
+            Listenfd.Listen(0);
+            Console.WriteLine("[服务器]启动成功");
+            while (true){
+                //Accept
+                Socket connfd = Listenfd.Accept();
+                Console.WriteLine("[服务器]Accept");
+                //Receive
+                byte[] readBuff = new byte[1024];
+                int count = connfd.Receive(readBuff);
+                string readStr = System.Text.Encoding.Default.GetString(readBuff, 0, count);
+                Console.WriteLine("[服务器]" + readStr);
+                //Send
+                string sendtime = System.DateTime.Now.ToString();
+                readStr = sendtime + ":" + readStr;
+                byte[] sendBytes = System.Text.Encoding.Default.GetBytes(readStr);
+                connfd.Send(sendBytes);
+            }
+        }
+    }
+}
