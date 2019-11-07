721b49f2d11c7662fa627895fdf254cf1f6aa140
JX-PC\JX <sijin1992@sina.com>
2019-11-07 22:04:25 +0800

    异步Echo

---
+++ Echo/Echo/Assets/Scripts/EchoAsync.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using UnityEngine.UI;
+
+public class EchoAsync : MonoBehaviour
+{
+    //定义套接字
+    Socket socket;
+    //UGUI
+    [SerializeField] private Button Connbutton;
+    [SerializeField] private Button Sendbutton;
+    public InputField InputFeld;
+    public Text text;
+    //接收缓冲区
+    byte[] readBuff = new byte[1024];
+    string recvStr = "";
+
+    private void Start()
+    {
+        Connbutton.onClick.AddListener(Connection);
+        Sendbutton.onClick.AddListener(Send);
+    }
+
+    //点击连接按钮
+    public void Connection()
+    {
+        //Socket
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        //Connect
+        socket.BeginConnect("127.0.0.1", 8888,ConnectCallback,socket);
+    }
+    //Connect回调
+    public void ConnectCallback(IAsyncResult ar){
+        try {
+            Socket socket = (Socket)ar.AsyncState;
+            socket.EndConnect(ar);
+            Debug.Log("Socket Connect Success");
+            //Receive
+            socket.BeginReceive(readBuff, 0, 1024, 0, ReceiveCallback, socket);
+        }
+        catch(SocketException ex){
+            Debug.Log("Socket Connect fail" + ex.ToString());
+        }
+    }
+    //Receive回调
+    public void ReceiveCallback(IAsyncResult ar){
+        try{
+            Socket socket = (Socket)ar.AsyncState;
+            int count = socket.EndReceive(ar);
+            recvStr = System.Text.Encoding.Default.GetString(readBuff, 0, count);
+            //递归Receive
+            socket.BeginReceive(readBuff, 0, 1024, 0, ReceiveCallback, socket);
+        }
+        catch (SocketException ex){
+            Debug.Log("Socket Receive fail" + ex.ToString());
+        }
+    }
+    //点击发送按钮
+    public void Send()
+    {
+        //Send
+        string sendStr = InputFeld.text;
+        byte[] sendBytes = System.Text.Encoding.Default.GetBytes(sendStr);
+        socket.BeginSend(sendBytes,0,sendBytes.Length,0,SendCallback,socket);
+    }
+    //Send回调
+    public void SendCallback(IAsyncResult ar) {
+        try
+        {
+            Socket socket = (Socket)ar.AsyncState;
+            int count = socket.EndSend(ar);
+            Debug.Log("Socket Send success" + count);
+        }
+        catch (SocketException ex)
+        {
+            Debug.Log("Socket Send fail" + ex.ToString());
+        }
+    }
+
+    public void Update()
+    {
+        text.text = recvStr;
+    }
+}
---
+++ Echo/EchoServer/EchoServerAsync/EchoServerAsync/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+
+namespace EchoServerAsync
+{
+    class ClientState
+    {
+        public Socket socket;
+        public byte[] readBuff = new byte[1024];
+    }
+
+    class MainClass
+    {
+        //监听Socket
+        static Socket listenfd;
+        //客户端Socket及状态信息
+        static Dictionary<Socket, ClientState> clients = new Dictionary<Socket, ClientState>();
+        static void Main(string[] args)
+        {
+            //Socket
+            listenfd = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //Bind
+            IPAddress ipAdr = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipEp = new IPEndPoint(ipAdr, 8888);
+            listenfd.Bind(ipEp);
+            //Listen
+            listenfd.Listen(0);
+            Console.WriteLine("[服务器]启动成功");
+            //Accept
+            listenfd.BeginAccept(AcceptCallback,listenfd);
+            //等待
+            Console.ReadLine();
+        }
+        //Accept回调
+        public static void AcceptCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Console.WriteLine("[服务器]Accept");
+                Socket listenfd = (Socket)ar.AsyncState;
+                Socket clientfd = listenfd.EndAccept(ar);
+                //clients列表
+                ClientState state = new ClientState();
+                state.socket = clientfd;
+                clients.Add(clientfd, state);
+                //接收数据BeginReceive
+                clientfd.BeginReceive(state.readBuff, 0, 1024, 0, ReceiveCallback, state);
+                //继续Accept
+                listenfd.BeginAccept(AcceptCallback, listenfd);
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("Socket Accept fail" + ex.ToString());
+            }
+        }
+        //Receive回调
+        public static void ReceiveCallback(IAsyncResult ar)
+        {
+            try
+            {
+                ClientState state = (ClientState)ar.AsyncState;
+                Socket clientfd = state.socket;
+                int count = clientfd.EndReceive(ar);
+                //客户端关闭
+                if (count == 0)
+                {
+                    clientfd.Close();
+                    clients.Remove(clientfd);
+                    Console.WriteLine("Socket Close");
+                    return;
+                }
+                string recvStr = System.Text.Encoding.Default.GetString(state.readBuff, 0, count);
+                string sendtime = System.DateTime.Now.ToString();
+                recvStr = sendtime + ":" + recvStr;
+                byte[] sendBytes = System.Text.Encoding.Default.GetBytes(recvStr);
+                //Send
+                clientfd.BeginSend(sendBytes, 0, sendBytes.Length, 0, SendCallback, clientfd);
+                //继续调用Receive
+                clientfd.BeginReceive(state.readBuff, 0, 1024, 0, ReceiveCallback, state);
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("Socket Receive fail" + ex.ToString());
+            }
+        }
+        //Send回调
+        public static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket socket = (Socket)ar.AsyncState;
+                int count = socket.EndSend(ar);
+                Console.WriteLine("Socket Send success" + " " + count.ToString());
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("Socket Send fail" + ex.ToString());
+            }
+        }
+    }
+}
