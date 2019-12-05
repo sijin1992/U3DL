850a6a709b709602eb42c030dead7e973512852d
JX-PC\JX <sijin1992@sina.com>
2019-12-05 10:49:27 +0800

    工具库

---
+++ MyEditor/MyEditor/Assets/Scripts/MyEditor/TextureChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TextureChange : AssetPostprocessor {
+    /*纹理图片导入的过程中，钩子函数调用顺序为：
+     * 进入导入阶段前：OnPreporcessTexture
+     * 导入阶段后：OnPostprocessTexture
+    */
+    void OnPostprocessTexture(Texture2D texture)
+    {
+        TextureImporter importer = assetImporter as TextureImporter;
+        Debug.Log(importer.assetPath);
+        //Set mipmap disable
+        importer.mipmapEnabled = false;
+        importer.textureType = TextureImporterType.Sprite;
+        //Save the changes
+        EditorUtility.SetDirty(importer);
+    }
+}
